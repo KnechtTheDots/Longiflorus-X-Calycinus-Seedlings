fdc1754b1fd50a32f7b83bf5edb773dd2e441b87
Josh Knecht <jknecht1@binghamton.edu>
2024-05-25 18:53:24 -0400

    jk

--- scripts/functions.R
+++ scripts/functions.R
@@ -1,3 +1,4 @@
+## functionto do the bayesian bootstrap and get mean and sd
 bayes_boot <- function(x){
   weights_mat <- gtools::rdirichlet(n = 1e4, alpha = rep(1, length(x)))
   mu <- apply(weights_mat, 1, function(z) sum(z*x))
@@ -5,6 +6,8 @@ bayes_boot <- function(x){
   return(data.frame(mu = mu, sd = sigma))
 }
 
+
+## generate the posterior predictive distributions
 
 post_pred <- function(df, trait){
   d <- df %>% 
@@ -48,13 +51,134 @@ post_pred <- function(df, trait){
   
   delta_pp <- c()
   for(i in 1:1e4){
-    lon_pp[i,] <- rnorm(length(lon), lon_boot$mu[i], lon_boot$sd[i])
-    cal_pp[i,] <- rnorm(length(cal), cal_boot$mu[i], cal_boot$sd[i])
-    f1_pp[i,] <- rnorm(length(f1), f1_boot$mu[i], f1_boot$sd[i])
-    f2_pp[i,] <- rnorm(length(f2), f2_boot$mu[i], f2_boot$sd[i])
+    lon_pp[i,] <- rgamma(length(lon), shape = (lon_boot$mu[i]^2)/(lon_boot$sd[i]^2), 
+                         rate = lon_boot$mu[i]/lon_boot$sd[i]^2)
+    cal_pp[i,] <- rgamma(length(cal), shape = (cal_boot$mu[i]^2)/(cal_boot$sd[i]^2), 
+                         rate = cal_boot$mu[i]/cal_boot$sd[i]^2)
+    f1_pp[i,] <- rgamma(length(f1), shape = (f1_boot$mu[i]^2)/(f1_boot$sd[i]^2), 
+                        rate = f1_boot$mu[i]/f1_boot$sd[i]^2)
+    f2_pp[i,] <- rgamma(length(f2), shape = (f2_boot$mu[i]^2)/(f2_boot$sd[i]^2), 
+                        rate = f2_boot$mu[i]/f2_boot$sd[i]^2)
+    
+    # lon_pp[i,] <- rlnorm(length(lon), meanlog = log(lon_boot$mu[i]) - .5*log((lon_boot$mu[i]^2 + lon_boot$sd[i]^2)/lon_boot$mu[i]^2),
+    #                      sdlog = sqrt(log((lon_boot$mu[i]^2 + lon_boot$sd[i]^2)/lon_boot$mu[i]^2)))
+    # cal_pp[i,] <- rlnorm(length(cal), meanlog = log(cal_boot$mu[i]) - .5*log((cal_boot$mu[i]^2 + cal_boot$sd[i]^2)/cal_boot$mu[i]^2),
+    #                      sdlog = sqrt(log((cal_boot$mu[i]^2 + cal_boot$sd[i]^2)/cal_boot$mu[i]^2)))
+    # f1_pp[i,] <- rlnorm(length(f1), meanlog = log(f1_boot$mu[i]) - .5*log((f1_boot$mu[i]^2 + f1_boot$sd[i]^2)/f1_boot$mu[i]^2),
+    #                     sdlog = sqrt(log((f1_boot$mu[i]^2 + f1_boot$sd[i]^2)/f1_boot$mu[i]^2)))
+    # f2_pp[i,] <- rlnorm(length(f2), meanlog = log(f2_boot$mu[i]) - .5*log((f2_boot$mu[i]^2 + f2_boot$sd[i]^2)/f2_boot$mu[i]^2),
+    #                     sdlog = sqrt(log((f2_boot$mu[i]^2 + f2_boot$sd[i]^2)/f2_boot$mu[i]^2)))
     
     delta_pp[i] <- mean(f2_pp[i,]) - (.25*(mean(lon_pp[i,]) + mean(cal_pp[i,])) + .5*mean(f1_pp[i,]))
   }
   return(list(lon_pp = lon_pp, cal_pp = cal_pp, f1_pp = f1_pp, f2_pp = f2_pp,
-              delta_pp = delta_pp))
+              delta_pp = delta_pp,
+              lon = lon,
+              cal = cal,
+              f1 = f1,
+              f2 = f2))
 }
+
+
+## pp predictive check plots
+
+post_pred_check <- function(df, trait){
+  d <- post_pred(df, trait)
+  
+  lon <- d$lon_pp
+  cal <- d$cal_pp
+  f1 <- d$f1_pp
+  f2 <- d$f2_pp
+  
+  mu <- sigma <- maximum <- minimum <- matrix(ncol = 4, nrow = 1e4)
+  line <- list(lon, cal, f1, f2)
+  for(i in 1:4){
+    mu[,i] <- apply(line[[i]], 1, mean)
+    sigma[,i] <- apply(line[[i]], 1, sd)
+    maximum[,i] <- apply(line[[i]], 1, max)
+    minimum[,i] <- apply(line[[i]], 1, min)
+  }
+  
+  mu <- data.frame(mu)
+  colnames(mu) <- c("lon", "cal", "f1", "f2")
+  
+  mu_p <- mu %>% 
+    pivot_longer(1:4, names_to = "line", values_to = "pp") %>% 
+    group_by(line) %>% 
+    summarise(upr = quantile(pp, .975),
+              lwr = quantile(pp, .025),
+              upr.5 = quantile(pp, .75),
+              lwr.5 = quantile(pp, .25)) %>% 
+    mutate(dat = c(mean(d$cal), mean(d$f1), mean(d$f2), mean(d$lon))) %>% 
+    ggplot(aes(x = line, y = dat)) +
+    geom_point(size = 3) +
+    geom_errorbar(aes(x = line, ymax = upr, ymin = lwr), width = 0) +
+    geom_errorbar(aes(x = line, ymax = upr.5, ymin = lwr.5), linewidth = 1, width = 0) +
+    theme_classic() +
+    labs(x = "Line", 
+         title = "Post. Pred. of the Mean", 
+         y = "")
+  
+  sigma <- sigma <- data.frame(sigma)
+  colnames(sigma) <- c("lon", "cal", "f1", "f2")
+  
+  sd_p <- sigma %>% 
+    pivot_longer(1:4, names_to = "line", values_to = "pp") %>% 
+    group_by(line) %>% 
+    summarise(upr = quantile(pp, .975),
+              lwr = quantile(pp, .025),
+              upr.5 = quantile(pp, .75),
+              lwr.5 = quantile(pp, .25)) %>% 
+    mutate(dat = c(sd(d$cal), sd(d$f1), sd(d$f2), sd(d$lon))) %>% 
+    ggplot(aes(x = line, y = dat)) +
+    geom_point(size = 3) +
+    geom_errorbar(aes(x = line, ymax = upr, ymin = lwr), width = 0) +
+    geom_errorbar(aes(x = line, ymax = upr.5, ymin = lwr.5), linewidth = 1, width = 0) +
+    theme_classic() +
+    labs(x = "Line", 
+         title = "Post. Pred. of the Std.Dev.",
+         y = "")
+  
+  maximum <- data.frame(maximum)
+  colnames(maximum) <- c("lon", "cal", "f1", "f2")
+  
+  max_p <- maximum %>% 
+    pivot_longer(1:4, names_to = "line", values_to = "pp") %>% 
+    group_by(line) %>% 
+    summarise(upr = quantile(pp, .975),
+              lwr = quantile(pp, .025),
+              upr.5 = quantile(pp, .75),
+              lwr.5 = quantile(pp, .25)) %>% 
+    mutate(dat = c(max(d$cal), max(d$f1), max(d$f2), max(d$lon))) %>% 
+    ggplot(aes(x = line, y = dat)) +
+    geom_point(size = 3) +
+    geom_errorbar(aes(x = line, ymax = upr, ymin = lwr), width = 0) +
+    geom_errorbar(aes(x = line, ymax = upr.5, ymin = lwr.5), linewidth = 1, width = 0) +
+    theme_classic() +
+    labs(x = "Line", 
+         title = "Post. Pred. of the Maximum",
+         y = "")
+  
+  minimum <- data.frame(minimum)
+  colnames(minimum) <- c("lon", "cal", "f1", "f2")
+  
+  min_p <- minimum %>% 
+    pivot_longer(1:4, names_to = "line", values_to = "pp") %>% 
+    group_by(line) %>% 
+    summarise(upr = quantile(pp, .975),
+              lwr = quantile(pp, .025),
+              upr.5 = quantile(pp, .75),
+              lwr.5 = quantile(pp, .25)) %>% 
+    mutate(dat = c(min(d$cal), min(d$f1), min(d$f2), min(d$lon))) %>% 
+    ggplot(aes(x = line, y = dat)) +
+    geom_point(size = 3) +
+    geom_errorbar(aes(x = line, ymax = upr, ymin = lwr), width = 0) +
+    geom_errorbar(aes(x = line, ymax = upr.5, ymin = lwr.5), linewidth = 1, width = 0) +
+    theme_classic() +
+    labs(x = "Line", 
+         title = "Post. Pred. of the Minimum",
+         y = "")
+  
+  cowplot::plot_grid(mu_p, sd_p, max_p, min_p, ncol = 2)
+}
+
